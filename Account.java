33e34a6c655c86f880c0298d5a49271ac0e052f2
amulyavm <amulya.mu@gmail.com>
2019-05-06 21:26:44 -0500

    Initial commit

---
+++ Account.java
@@ -0,0 +1,26 @@
+package bankOA;
+
+public class Account {
+
+	public static void main(String[] args) {
+		Bank anu = new Bank();
+
+		anu.deposite(1000);
+		anu.withdraw(55.5);
+		anu.intrest();
+		
+		Bank ram = new Bank();
+		ram.deposite(10000);
+		ram.withdraw(6500.50);
+		ram.intrest();
+		
+		anu.deposite(500);
+		ram.intrest();
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
